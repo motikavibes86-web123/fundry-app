01884fd47f3da5e388f8271d7b2b79e5cb58a524
motikavibes86-web123 <252194679+motikavibes86-web123@users.noreply.github.com>
2026-01-12 17:53:00 +0300

    Update config.php

--- src/Fire base/Fire base/config.php
+++ src/Fire base/Fire base/config.php
@@ -1,1 +1,14 @@
 
+<?php
+include 'config.php';
+require_once 'vendor/autoload.php';
+
+use Twilio\Rest\Client;
+
+$client = new Client(TWILIO_SID, TWILIO_TOKEN);
+
+// Tuma SMS
+$client->messages->create('+2557xxxxxxx', [
+    'from' => TWILIO_FROM,
+    'body' => 'Hello, hii ni SMS ya Motika Vibes!'
+]);
