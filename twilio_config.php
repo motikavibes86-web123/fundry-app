34f28bd3aed1cecf4903bf571e8495ba6b97fafd
motikavibes86-web123 <252194679+motikavibes86-web123@users.noreply.github.com>
2026-01-12 17:51:23 +0300

    Create twilio_config.php

---
+++ twilio_config.php
@@ -0,0 +1,6 @@
+<?php
+// Twilio Credentials
+define('TWILIO_SID', 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+define('TWILIO_TOKEN', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+define('TWILIO_FROM', '+255750020767'); // namba ya Twilio
+?>
